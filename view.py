d323529f2ec145e6ab097dc1cdfa57257e767840
Alex <best1972@list.ru>
2023-03-23 20:42:44 +0300

    recess

--- view.py
+++ view.py
@@ -1,25 +1,24 @@
-
-
+import textoffields
 
 def mainMenu() -> int:
-    return int(input('''Главное меню:
-    1. Открыть файл
-    2. Сохранить файл
-    3. Показать все контакты
-    4. Добавить контакт в справочник Изменить контакт
-    5. Найти контакт в справочнике Удалить контакт
-    6. Выйти
-    Выберите пункт меню: '''))
-
+    print(textoffields.mainMenu)
+    lengthMenu = len(textoffields.mainMenu.split('\n')) - 1 
+    while True:
+        choice = input('Выберите пункт меню: ')
+        if choice.isdigit() and 0 <int(choice)<= lengthMenu:
+            return int(choice)
+        else:
+            print(f'Введите число от 1 до {lengthMenu}')
+    
 def showContacs(book: list[dict], errorMessage: str):
     if not book:
         print(errorMessage)
         return False
     else:
         for i, contact in enumerate(book, 1):
-            print(f'{i}. {contact.get("name"):<20}'
-                f'{contact.get("phone"):<20}'
-                f'{contact.get("comment"):<20}')
+            print(f'{i}. {contact.get("name"):<20} '
+            f'{contact.get("phone"):<20} '
+            f'{contact.get("comment"):<20}')
         return True
 
 def addContact () -> dict:
@@ -28,4 +27,20 @@ def addContact () -> dict:
     comment = input('Введите комментарий: ')
     return {'name': name, 'phone': phone, 'comment': comment}
 
+def inputIndex(message: str):
+    return int(input(message))
 
+def inputSearch(message):
+    return input(message)
+
+def changeContact(book: list[dict], index: int):
+    print('Введите новые данные или оставьте пустое поле, для выхода без изменений')
+    contact = addContact()
+    return {'name': contact.get('name') if contact.get('name') else book[index-1].get('name'),
+        'phone': contact.get('phone') if contact.get('phone') else book[index-1].get('phone'),
+        'phone': contact.get('comment') if contact.get('comment') else book[index-1].get('comment')}
+    
+def showMessage(message: str):
+    print('-'*20)
+    print(message)
+    print('-'*20)
