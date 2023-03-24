c875414d3f892ef7b4f19cf802442faf998199c9
Alex <best1972@list.ru>
2023-03-24 19:01:04 +0300

    end

--- Controller.py
+++ Controller.py
@@ -16,12 +16,14 @@ def start():
                 view.showContacs(pb, 'Телефонная книга пуста или не открыта')
             case 4:
                 model.addContact(view.addContact())
+                view.showMessage('Выберете пункт 2 для сохранения внесенных изменений в файле')
             case 5:
                 if view.showContacs(pb, 'Телефонная книга пуста или не открыта'):
                     index = view.inputIndex('Введите порядковый номер изменяемого контакта: ')
                     contact = view.changeContact(pb, index)
                     model.changeContact(contact, index)
                     view.showMessage(f'Контакт {model.getPhoneBook()[index-1].get("name")} успешно изменен!')
+                    view.showMessage('Выберете пункт 2 для сохранения внесенных изменений в файле')
             case 6:
                 search = view.inputSearch('Введите искомый элемент: ')
                 result = model.findContact(search)
@@ -32,5 +34,6 @@ def start():
                 contact = view.deleteContact(pb, index)
                 model.deleteContact(contact, index)
                 view.showMessage('Контакт успешно удален!')
+                view.showMessage('Выберете пункт 2 для сохранения внесенных изменений в файле')
             case 8:
                 return
